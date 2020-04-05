a9696782aa8263fa2be56433e033657dba788150
KirkAdlao <60393974+KirkAdlao@users.noreply.github.com>
2020-04-04 22:08:49 -0400

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+/* This program is a simple code example of a for loop.*/
+public class ForLoop {
+
+    public static void main(String args[]){
+
+         for(int i=10; i>1; i--){
+              System.out.println("The value of i is: "+i);
+         }
+    }
+}
